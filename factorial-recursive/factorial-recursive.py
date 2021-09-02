bebb0483fec128cd308137d10ff44fb4fcc30d2d
ivanrezm <ivanrezm@gmail.com>
2021-09-02 15:05:58 +0200

    factorial.py more redeable

--- factorial-recursive/factorial-recursive.py
+++ factorial-recursive/factorial-recursive.py
@@ -2,17 +2,20 @@
 
 import sys
 
+# Constant
 numArgsNeeded = 2
 
+# Functions
 def factorial(number):
-	if number > 1:
+	if number > 0:
 		number = number*factorial(number-1)
-		return number
-	else:
-		return number
-
+	elif number == 0:
+		number = 1
+	return number
+# Main
 if __name__ == "__main__":
 	if len(sys.argv) < numArgsNeeded:
-		print("You need more arguments")
+		print("You need one argument")
+		print(">> ./factorial.py <number>")
 	else:
 		print(factorial(int(sys.argv[1])))
